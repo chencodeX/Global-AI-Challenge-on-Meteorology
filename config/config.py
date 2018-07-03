da6c2a7c3b8a45a479e2a5d23ecc3733b4e129f6
zihao.chen_tp <zihao.chen@moji.com>
2018-07-03 12:10:36 +0800

    debug

--- config/config.py
+++ config/config.py
@@ -12,6 +12,6 @@ import os
 RADAR_TIME_INTERVAL = 360 * 5
 DATA_PATH ="/home/meteo/zihao.chen/data/IEEE_ICDM_2018/download/"
 PATH_TEST_FILE_PATH = os.path.join(DATA_PATH,'test_file_001/SRAD2018_Test_1/')
-PATH_PREV_FILE_PATH = os.path.join(DATA_PATH,'extrapolate_flow')
+PATH_PREV_FILE_PATH = os.path.join(DATA_PATH,'extrapolate_flow/')
 
 TIME_RADAR_EXTRAPOLATE = 6
