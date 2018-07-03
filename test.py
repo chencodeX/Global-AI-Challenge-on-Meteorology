7dd75f4bb6b858c24b1f63deb5224d8bfca3aabe
zihao.chen_tp <zihao.chen@moji.com>
2018-07-03 11:47:09 +0800

    add spopt

---
+++ test.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Copyright (c) 2017 - zihao.chen <zihao.chen@moji.com>
+'''
+Author: zihao.chen
+Create Date: 2018-07-03
+Modify Date: 2018-07-03
+descirption: ""
+'''
+
+
+from utils.flow_extrapolate import Extrapolate
+
+ep = Extrapolate()
+ep.transform('/home/meteo/zihao.chen/data/IEEE_ICDM_2018/download/test_file_001/SRAD2018_Test_1/RAD_296682434212531/RAD_296682434212531_030.png')
