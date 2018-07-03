ba35ccfbdc7c097a1301069cd2dbc130880b3a90
zihao.chen_tp <zihao.chen@moji.com>
2018-07-03 20:10:55 +0800

    debug

--- utils/flow_extrapolate.py
+++ utils/flow_extrapolate.py
@@ -76,7 +76,7 @@ class Extrapolate(object):
                 print 2
                 self.data_type = 2
                 self.error_data = img_tmp
-                return img_tmp[22:-22,22:-22]
+                return img_tmp[22:-22,6:-6]
             self.data_type = 0
             img_tmp[img_tmp == 255] = 0
             return img_tmp
@@ -205,7 +205,7 @@ class Extrapolate(object):
             images[images == 0] = 255
             images = np.concatenate((images,self.error_data),axis=0)
         elif self.data_type == 2:
-            self.error_data[22:-22, 22:-22] = images
+            self.error_data[22:-22, 6:-6] = images
             return self.error_data.copy()
         else:
             images[images == 0] = 255
