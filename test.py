de360381abac2825e8bf349418b533d081e78f7f
zihao.chen_tp <zihao.chen@moji.com>
2018-07-04 12:12:08 +0800

    debug

--- test.py
+++ test.py
@@ -9,19 +9,31 @@ descirption: ""
 '''
 
 import os
-
+import multiprocessing
 from utils.flow_extrapolate import Extrapolate
 from config.config import *
 work_list= os.listdir(PATH_TEST_FILE_PATH)
 type_count = {0:0,1:0,2:0}
 
+def extrapolate_single(file_path):
+    ep = Extrapolate()
+    ep.transform(file_path)
+    print ep.data_type
+
+
+all_param = []
+
+
 for work_name in work_list:
     work_path = os.path.join(PATH_TEST_FILE_PATH,work_name)
     work_file_path = os.path.join(work_path,work_name+'_030.png')
     print work_file_path
-    ep = Extrapolate()
-    ep.transform(work_file_path)
-    print ep.data_type
-    type_count[ep.data_type]+=1
+    all_param.append(work_file_path)
 
+    # type_count[ep.data_type]+=1
+pool = multiprocessing.Pool(14)
+
+pool.map(extrapolate_single, all_param)
+pool.close()
+pool.join()
 print type_count
