82719607f88c392e499c9cc22a4af130ff5e60cf
zihao.chen_tp <zihao.chen@moji.com>
2018-07-05 19:50:02 +0800

    out 6

--- test_upload.py
+++ test_upload.py
@@ -11,16 +11,16 @@ import os
 import shutil
 from config.config import *
 
-test_path = PATH_PREV_FILE_PATH
+test_path = '/home/meteo/zihao.chen/data/IEEE_ICDM_2018/download/SRAD2018_submit_sample_2018070414'
 P_path = os.path.join(DATA_PATH,'extrapolate_flow_merge')
 samples = os.listdir(test_path)
 
 for sample in samples:
     temp_data_path = os.path.join(test_path,sample)
-    temp_last_image_path = os.path.join(temp_data_path,sample+'_f005.png')
+    temp_last_image_path = os.path.join(temp_data_path,sample+'_f006.png')
     if os.path.exists(temp_last_image_path):
         up_load_data_path = temp_data_path.replace('extrapolate_flow','extrapolate_flow_merge')
         if not os.path.exists(up_load_data_path):
             os.makedirs(up_load_data_path)
-        save_path = os.path.join(up_load_data_path,sample+'_f00%d.png'%(1))
+        save_path = os.path.join(up_load_data_path,sample+'_f00%d.png'%(6))
         shutil.copy(temp_last_image_path,save_path)
