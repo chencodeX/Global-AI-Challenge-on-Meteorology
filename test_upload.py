4fc21859341894e3f2301a8bbd24401e94d01228
zihao.chen_tp <zihao.chen@moji.com>
2018-07-05 16:22:38 +0800

    stpe 5,out 5

--- test_upload.py
+++ test_upload.py
@@ -11,16 +11,15 @@ import os
 import shutil
 from config.config import *
 
-test_path = os.path.join(DATA_PATH,'SRAD2018_Test_1')
-P_path = os.path.join(DATA_PATH,'up_test')
+test_path = PATH_PREV_FILE_PATH
+P_path = os.path.join(DATA_PATH,'extrapolate_flow_merge')
 samples = os.listdir(test_path)
 
 for sample in samples:
     temp_data_path = os.path.join(test_path,sample)
-    temp_last_image_path = os.path.join(temp_data_path,sample+'_030.png')
+    temp_last_image_path = os.path.join(temp_data_path,sample+'_f005.png')
     if os.path.exists(temp_last_image_path):
-        up_load_data_path = temp_data_path.replace('SRAD2018_Test_1','up_test')
+        up_load_data_path = temp_data_path.replace('extrapolate_flow','extrapolate_flow_merge')
         os.makedirs(up_load_data_path)
-        for i in range(6):
-            save_path = os.path.join(up_load_data_path,sample+'_f00%d.png'%(i+1))
-            shutil.copy(temp_last_image_path,save_path)
+        save_path = os.path.join(up_load_data_path,sample+'_f00%d.png'%(5))
+        shutil.copy(temp_last_image_path,save_path)
